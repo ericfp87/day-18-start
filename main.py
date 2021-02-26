cee5a6efe0cd732324a21883f857e5b5c59fd318
ericfp87 <52512077+ericfp87@users.noreply.github.com>
2021-02-26 11:02:25 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+#####Turtle Intro######
+from turtle import Screen
+import turtle as t
+
+
+tim = t.Turtle()
+tim.shape("turtle")
+tim.color("red")
+tim.forward(100)
+tim.setheading(90)
+tim.forward(100)
+tim.setheading(180)
+tim.forward(100)
+tim.setheading(270)
+tim.forward(100)
+
+
+# timmy_the_turtle.right(0)
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.right(100)
+# timmy_the_turtle.right(100)
+# timmy_the_turtle.setheading(0)
+screen = Screen()
+screen.exitonclick()
+
+######## Challenge 1 - Draw a Square ############
